ccdf146e1608dd4805a95d0f78c56760b596c657
j.developer@meddatacarepro.com <pass1234>
2017-02-08 15:43:45 +0530

    test

---
+++ ConsoleApplication1/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+  public  class Class1
+  {
+      public delegate int Print(int value, int value2);
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+
+ public  Print print = delegate(int val, int val2)
+        {
+            return val + val2;
+        };
+
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+namespace ConsoleApplication1
+{
+    class Program
+    {
+       
+       
+    
+        static void Main(string[] args)
+        {
+            Class1 cls = new Class1();
+
+            Stopwatch stopWatch = new Stopwatch();
+                    
+           
+            stopWatch.Start();
+               
+           cls.add(2, 34);
+
+            //cls.print(2, 34);
+            stopWatch.Stop();
+
+            Console.Write(stopWatch.ElapsedTicks);
+            Console.Read();
+
+         
+        }
+        
+    
+    }
+}
